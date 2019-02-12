e3329b9c8c4260d825caef17424b1856737a392b
Ricx8 <marquessricardo@gmail.com>
2019-02-12 00:57:28 -0500

    ServerSide: Database updated.

--- src/web/database/shoes9G.sql
+++ src/web/database/shoes9G.sql
@@ -1,8 +1,7 @@
 -- phpMyAdmin SQL Dump
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Feb 06, 2019 at 12:54 AM
+-- Generation Time: Feb 12, 2019 at 12:55 AM
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -27,8 +26,8 @@ CREATE TABLE `logs` (
   `id` bigint(20) NOT NULL,
   `trackerID` int(11) DEFAULT NULL,
   `dateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `latitude` float NOT NULL DEFAULT '0',
-  `longitude` int(11) NOT NULL DEFAULT '0'
+  `latitude` double NOT NULL DEFAULT '0',
+  `longitude` double NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -40,6 +39,7 @@ CREATE TABLE `logs` (
 CREATE TABLE `trackers` (
   `id` int(11) NOT NULL,
   `name` text,
+  `pass` varchar(255) DEFAULT NULL,
   `status` enum('online','offline') DEFAULT NULL,
   `lastTrack` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -68,12 +68,12 @@ ALTER TABLE `trackers`
 -- AUTO_INCREMENT for table `logs`
 --
 ALTER TABLE `logs`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
 --
 -- AUTO_INCREMENT for table `trackers`
 --
 ALTER TABLE `trackers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
