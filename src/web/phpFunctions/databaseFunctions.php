371a97028746b5f91c98b568ffcb381fc9da9db9
Ricx8 <marquessricardo@gmail.com>
2019-02-12 00:15:45 -0500

    ServerSide: inserting latest location with mySQL db

--- src/web/phpFunctions/databaseFunctions.php
+++ src/web/phpFunctions/databaseFunctions.php
@@ -1,30 +1,20 @@
 <?php  
-	$dbhost = 'localhost';
+	$dbhost = "localhost";
 	$dbuser = 'YOUR_DB_USER';
 	$dbpass = 'YOUR_DB_PASSWORD';
-	$conn = NULL;
-
-	function connectToDatabase(){
-		echo "HELLO11";
-		$conn = new mysqli($dbhost, $dbuser, $dbpass);
-		$r = 0;
-echo "HELLO";
-		if(!$conn->connect_error) $r = 1;
-echo "HELLO $r";
-		return ($r);
-	}
+	$dbname = "shoes9G";
 
 	function insertDB($sql){
+		// Create connection
+		$conn = new mysqli($GLOBALS['dbhost'], $GLOBALS['dbuser'], $GLOBALS['dbpass'], $GLOBALS['dbname']);
 		$r = 0;
-		if ($conn->query($sql) === TRUE) {
-			$r = 1;
-		}
+
+		if(!$conn->connect_error)
+			if ($conn->query($sql) === TRUE) $r = 1;
+		else $r = -1;
+		
+		$conn->close();
 
 		return ($r);
-	}
-
-	function closeConnection(){
-		$conn->close();
-		$conn = NULL;
 	}	
 ?>
--- src/web/setLocationPOST.php
+++ src/web/setLocationPOST.php
@@ -5,23 +5,17 @@
 	$latitude = $_POST["latitude"];
 	$longitude = $_POST["longitude"];
 
-	if (connectToDatabase()){
-		$result = NULL;
-		
-		$result = insertDB("
-			INSERT INTO logs (trackerID, latitude, longitude)
-			VALUES (
-				(SELECT id FROM trackers WHERE pass = '$pass'),
-				$latitude,
-				$longitude
-			)
-		");
+	$result = insertDB("
+		INSERT INTO logs (trackerID, latitude, longitude)
+		VALUES (
+			(SELECT id FROM trackers WHERE pass = '$pass'),
+			$latitude,
+			$longitude
+		)
+	");
 
-		closeConnection();
-
-		if ($result == 1) echo "1\r\n\r\n";
-		else echo "0\r\n\r\n";
-	}
+	if ($result == 1) echo "1\r\n\r\n";
+	else echo "0\r\n\r\n";
 
 	//exec("echo '".date('Y-m-d h:i:sa').";$latitude;$longitude' >> location.txt");
 ?>
